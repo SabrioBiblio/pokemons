cf08d778acd29f0dc2cb7314152c21e68bcdba8b
SabrioBiblio <fheronax@gmail.com>
2023-06-04 02:14:07 +0300

    Change basename to /pokemons

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,7 @@ import './reset-style.scss'
 import DetailPage from './pages/DetailPage/DetailPage.tsx';
 import HomePage from './pages/HomePage/HomePage.tsx';
 import { store } from './redux/store.ts';
+
 import './styles/main-style.scss';
 
 const router = createBrowserRouter([
@@ -22,13 +23,13 @@ const router = createBrowserRouter([
   {
     path: '/detail',
     element: <DetailPage/>
-  }
-])
+  },
+], { basename: '/pokemons' })
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,18 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: '@components', replacement: path.resolve(__dirname, 'src/components') }],
-  },
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+    resolve: {
+      alias: [{ find: '@components', replacement: path.resolve(__dirname, 'src/components') }],
+    },
+  }
+
+  if (command !== 'serve') {
+    config.base = '/pokemons/'
+  }
+
+  return config
 })
