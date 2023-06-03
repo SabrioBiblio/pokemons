c04c7faa18e58f5afd061fdaf228ee1da04f2b4b
SabrioBiblio <fheronax@gmail.com>
2023-06-04 01:59:26 +0300

    Change vite config for GHPages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pokemons/',
   resolve: {
     alias: [{ find: '@components', replacement: path.resolve(__dirname, 'src/components') }],
   },
