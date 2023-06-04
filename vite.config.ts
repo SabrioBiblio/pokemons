17d40c2dbccf8fcf52fdb99f2ba51addde418068
SabrioBiblio <fheronax@gmail.com>
2023-06-04 10:15:25 +0300

    Add homepage to package.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   const config = {
     plugins: [react()],
     base: '/pokemons',
     resolve: {
       alias: [{ find: '@components', replacement: path.resolve(__dirname, 'src/components') }],
     },
-  }
-
-  if (command !== 'serve') {
-    config.base = '/pokemons/'
   }
 
   return config
